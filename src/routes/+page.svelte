41b302d69105f71388ffccc65ea9b48faef899be
AlexandreHiroyuki <alex.hiroyuki@outlook.com>
2025-07-18 17:57:22 -0300

    build: add skeleton dependency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="flex h-screen w-full items-center justify-center bg-black">
+	<div class="flex aspect-[9/19.5] w-full max-w-[414px] items-center justify-center bg-white">
+		<button
+			class="flex h-16 w-16 items-center justify-center rounded-full bg-blue-500 p-4 text-white transition-colors hover:bg-blue-600"
+		>
+			Click me
+		</button>
+	</div>
+</main>
