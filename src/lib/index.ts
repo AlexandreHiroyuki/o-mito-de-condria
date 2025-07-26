181b1060ee6b87b612eec16e2f9af21e3c8be6fc
Alexandre Hiroyuki <alex.hiroyuki@outlook.com>
2025-07-26 03:48:44 -0300

    fix: add index.ts to the lib directory to fix the build

---
+++ src/lib/index.ts
@@ -0,0 +1,15 @@
+// Game state and logic
+export type * from './game.d.ts';
+export * from './game.svelte.js';
+
+// Game upgrades system
+export * from './game.upgrades.svelte.js';
+
+// Game discoveries system
+export * from './game.discoveries.svelte.js';
+
+// Utility functions
+export * from './formatter.js';
+
+// Toaster instance
+export * from './toaster.svelte.js';
